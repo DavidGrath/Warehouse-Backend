05b8b49e289e17ccdc17e13d4e9936551f5bfa14
DavidGrath <davidadeyyinka@gmail.com>
2020-05-01 16:04:45 +0100

    Local Commit

--- src/main/java/app/models/entities/Warehouse.java
+++ src/main/java/app/models/entities/Warehouse.java
@@ -1,9 +1,14 @@
 package app.models.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Warehouse {
     public Warehouse() {
     }
+
+    @Id
+    Long id;
+    String name;
 }
